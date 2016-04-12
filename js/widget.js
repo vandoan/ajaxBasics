93c6262ae2f7b8ba0b9b4e0ef0b42bdbb0a99e2b
Van Doan <vdoan09@gmail.com>
2016-04-11 21:40:24 -0400

    ajax 2

--- js/widget.js
+++ js/widget.js
@@ -26,3 +26,38 @@ xhr.onreadystatechange = function () {
 xhr.open('GET', 'data/employees.json');
 // Step 4: send the XMLHttpRequest
 xhr.send();
+
+
+
+
+
+
+
+// Create a xml object
+var xhr2 = new XMLHttpRequest();
+// Ste 2: Call back function 
+xhr2.onreadystatechange = function () {
+	if (xhr2.readyState === 4){
+		// console.log(xhr.responseText);
+		// console.log(typeof xhr.responseText);
+		var rooms = JSON.parse(xhr2.responseText);
+		// console.log(typeof employees);
+		 console.log(rooms);
+
+		var statusHTML = '<ul class="rooms">';
+		for (var i=0; i<rooms.length; i+=1){
+			if (rooms[i].available === true){
+				statusHTML += '<li class="empty">';
+			} else {
+				statusHTML += '<li class="full">';
+			}
+			statusHTML += rooms[i].room;
+		}
+			statusHTML += '</ul>';
+			document.getElementById('roomList').innerHTML = statusHTML;
+	}
+};
+// Step 3: open a request
+xhr2.open('GET', 'data/rooms.json');
+// Step 4: send the XMLHttpRequest
+xhr2.send();
